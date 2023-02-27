a6e006c1dd64289778b89e8c81e0063bf22d7ebd
dlprk-qa32 <104550522+dlprk-qa32@users.noreply.github.com>
2023-02-27 13:29:52 +0530

    Create test.tf

---
+++ scan/test.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "bar" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+  monitoring    = true
+
+  metadata_options {
+      http_endpoint = "disabled"    
+      http_tokens = "required"
+  }
+}
