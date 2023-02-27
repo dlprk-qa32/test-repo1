f6ab44bc7b816de956c4d201c5ea55cb00e02c02
dlprk-qa32 <104550522+dlprk-qa32@users.noreply.github.com>
2023-02-27 13:35:32 +0530

    Update test.tf

--- scan/test.tf
+++ scan/test.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bar" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = "ami-005e54dee72cc1d00"
   instance_type = "t2.micro"
   monitoring    = true
 
