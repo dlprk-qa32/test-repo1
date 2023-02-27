c9f6d576b2211621af5320013293b7cd61bb4c7f
dlprk-qa32 <104550522+dlprk-qa32@users.noreply.github.com>
2023-02-27 16:33:13 +0530

    Update ec2_monitoring.tf

--- ec2_monitoring.tf
+++ ec2_monitoring.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "app_server333" {
+resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   ingress_cidr_blocks = ["0.0.0.0/16"]
