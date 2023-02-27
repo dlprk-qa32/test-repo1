8aa94c270c96bf397e73c156e03a295fc959b648
dlprk-qa32 <104550522+dlprk-qa32@users.noreply.github.com>
2023-02-27 14:12:14 +0530

    Add files via upload

---
+++ ec2_monitoring.tf
@@ -0,0 +1,10 @@
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  monitoring = false
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
